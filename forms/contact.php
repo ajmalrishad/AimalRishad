89ac5cff96e35d60ece2bcb5ca8991db7f706afb
ajmalrishad <69522541+ajmalrishad@users.noreply.github.com>
2021-11-03 08:41:01 +0530

    Update contact.php

--- forms/contact.php
+++ forms/contact.php
@@ -2,15 +2,14 @@
 <?php
   $name = htmlspecialchars($_POST['name']);
   $email = htmlspecialchars($_POST['email']);
-  $phone = htmlspecialchars($_POST['phone']);
-  $website = htmlspecialchars($_POST['website']);
+  $website = htmlspecialchars($_POST['subject']);
   $message = htmlspecialchars($_POST['message']);
 
   if(!empty($email) && !empty($message)){
     if(filter_var($email, FILTER_VALIDATE_EMAIL)){
-      $receiver = "receiver_email_address"; //enter that email address where you want to receive all messages
+      $receiver = "ajmalaju618@gmail.com"; //enter that email address where you want to receive all messages
       $subject = "From: $name <$email>";
-      $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\nMessage:\n$message\n\nRegards,\n$name";
+      $body = "Name: $name\nEmail: $email\nSubject: $subject\n\nMessage:\n$message\n\nRegards,\n$name";
       $sender = "From: $email";
       if(mail($receiver, $subject, $body, $sender)){
          echo "Your message has been sent";
